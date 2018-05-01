248757a2f510ea07751e9258d9c0bf548e3d6144
cblgh <cblgh@cblgh.org>
2018-05-01 17:31:36 +0200

    cleanup input further

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 var readline = require("readline")
+
 function readInput(db) {
     var rl = readline.createInterface({
         input: process.stdin,
@@ -6,14 +7,9 @@ function readInput(db) {
         terminal: false
     })
 
-
     function logResult(err, result) {
-        if (err) {
-            console.error("hyperdb failed with", err)
-        }
-        if (arguments.length >= 2) {
-            console.log(result)
-        }
+        if (err) { console.error("hyperdb failed with", err) }
+        if (arguments.length >= 2) { console.log(result) }
     }
     
     var pattern = (/\.(\w*)\s*(.*)/)
@@ -21,20 +17,14 @@ function readInput(db) {
         var match = pattern.exec(line) 
         var cmd = match[1] || ""
         var arg = match[2] || ""
-        if (cmd == "get") {
-            db.get(arg, logResult)
-        } else if (cmd === "put") {
+        if (cmd === "put") {
             var [key, value] = arg.split("=")
             db.put(key, value, logResult)
-        } else if (cmd === "auth") {
-            db.authorize(Buffer.from(arg, "hex"), logResult)
-        } else if (cmd === "local") {
-            console.log("local key is\n\t", db.local.key.toString("hex"))
-        } else if (cmd === "db") {
-            console.log("db key is\n\t", db.key.toString("hex"))
-        } else if (cmd === "registered") {
-            db.authorized(Buffer.from(arg, "hex"), logResult)
-        }
+        } else if (cmd === "get") { db.get(arg, logResult)
+        } else if (cmd === "auth") { db.authorize(Buffer.from(arg, "hex"), logResult)
+        } else if (cmd === "local") { console.log("local key is\n\t", db.local.key.toString("hex"))
+        } else if (cmd === "db") { console.log("db key is\n\t", db.key.toString("hex"))
+        } else if (cmd === "registered") { db.authorized(Buffer.from(arg, "hex"), logResult) }
     })
 }
 module.exports = readInput
