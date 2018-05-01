324005128d6ecbf66792893d855c037b5285105e
cblgh <cblgh@cblgh.org>
2018-05-01 18:03:51 +0200

    cleanup index.js further

--- index.js
+++ index.js
@@ -6,75 +6,63 @@ var minimist = require("minimist")
 
 var args = minimist(process.argv.slice(2))
 
+if (!args.db) {
+    console.error("error: need --db flag!\nexamples:\n\tnode index.js --db <your-new-db-file>\n\tnode index.js --db my.db")
+    return
+}
+
+// check if args.key was provided in the cli using --key
 var db = args.key ? 
+    // join an existing hyperdb where args.key comes from providing index.js with --key <key>
     hyperdb(args.db, args.key, { valueEncoding: "utf-8", reduce: (a, b) => a }) :
+    // or create a new original hyperdb, by not specifying a key
     hyperdb(args.db, { valueEncoding: "utf-8", reduce: (a, b) => a })
 
-
 db.on("ready", function() {
-    console.log("db ready!!")
+    console.log("db ready!\ndb key is\n\t", db.key.toString("hex"))
+    console.log("local key is\n\t", db.local.key.toString("hex"))
     readInput(db)
     var swarm = setupSwarm(db)
 })
 
-// LESSONS
-// * pass the key as a string to hyperdb(<storage>, key)
-// * connect to db.key, authorize using db.local.key
-// * db.authorize / db.authorized doesn't accept a string as key; needs buffer
-
-// TODO:
-// * CLEAN the code
-// * DOCUMENT what i am doing
-// * READ substack's mesh code and diff with mine
-// * USE hyperdiscovery instead
-
-// HYPERDB ISSUES:
-// * peer has been authorized but the db changes do not propagate from the authorized peer to the og creator's db
-// * constant connection events from a single peer that has joined
-// * when on same computer but in different folders, and with an authorized peer, and after having written once and then
-//   restarted the node process i get: 
-//   "Error: Another hypercore is stored here"
-//
+// join a discovery swarm, connecting this hyperdb instance with others over DHT & DNS
+// this step is essential to have your database changes propagate between your db and that of others
 function setupSwarm(db) {
     var dbstr = db.key.toString("hex")
     var swarm = discovery(swarmDefaults({
         id: dbstr,
         stream: function(peer) {
-            console.log("swarm.stream peer")
             return db.replicate({ // TODO: figure out what this truly does
                 live: true,
                 userData: db.local.key
             }) 
         }
     }))
-    console.log("swarm key", dbstr)
+    console.log("looking for peers using swarm id\n\t", dbstr)
+
     swarm.join(dbstr)
+
+    // emitted when a new peer joins 
     swarm.on("connection", (peer) => {
+        // initiate auto-authorization: 
+        // use the local key from the peer, stored in their userData, to authenticate them automatically
+        // (thanks substack && JimmyBoh https://github.com/karissa/hyperdiscovery/pull/12#pullrequestreview-95597621 )
         if (!peer.remoteUserData) {
             console.log("peer missing user data")
             return
         }
-        console.log(peer.remoteUserData)
-        console.log(peer.remoteUserData.toString("hex"))
-        try { var key = Buffer.from(peer.remoteUserData) }
+        try { var remotePeerKey = Buffer.from(peer.remoteUserData) }
         catch (err) { console.error(err); return }
-        db.authorized(key, function (err, auth) {
-            console.log(key.toString("hex"), "authorized: " + auth)
+
+        db.authorized(remotePeerKey, function (err, auth) {
+            console.log(remotePeerKey.toString("hex"), "authorized? " + auth)
             if (err) return console.log(err)
-            if (!auth) db.authorize(key, function (err) {
+            if (!auth) db.authorize(remotePeerKey, function (err) {
                 if (err) return console.log(err)
+                console.log(remotePeerKey.toString("hex"), "was just authorized!")
             })
         })
     })
+    // return the swarm instance
     return swarm
 }
-
-
-db.put("/hello", "world", function (err) {
-    if (err) throw err
-    db.get("/hello", function (err, node) {
-        if (err) throw err
-        console.log("/hello --> " + node.value)
-    })
-})
-
